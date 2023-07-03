fcef23fafb71fafc21d82d8f37c2998d2cee316d
AbdulDefi <85731802+AbdulDefi@users.noreply.github.com>
2023-07-03 18:28:00 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,3 @@
-import { ContentProvider } from 'destack'
 import 'grapesjs/dist/css/grapes.min.css'
-
 export { getStaticProps } from 'destack/build/server'
-
-export default function Page(props) { 
-    return (
-        <div style={{height: '100%'}}>
-            <span>Hello world</span>
-            <ContentProvider {...props}/>
-        </div>)
-}
+export { ContentProvider as default } from 'destack'
